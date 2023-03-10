9f248c175ef8cd29cc08f86d3b7d6e6b0b7e3249
Zivojin <zivojin.davidovic2001@gmail.com>
2023-03-10 23:50:15 +0100

    database-connection

--- actions/database.php
+++ actions/database.php
@@ -1,1 +1,12 @@
 <?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try{
+    $connection = new PDO("mysql:host=$servername;dbname=phpsmn", $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch (PDOException $ex){
+    echo "Connection failed: " . $ex->getMessage();
+}
